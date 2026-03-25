262a092e5e478361f9808f7b89cc239f3f3525fb
foodtruck-53 <samuelxs238@gmail.com>
2026-03-24 22:06:46 -0700

    Update 2

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Samuel Xiao" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hey everyone! I'm Samuel from the Silicon Valley, California. I'm currently a junior in high school and enjoy a variety of video games with my friends. I also like listening to many different types of interesting music.")
 
 if __name__ == "__main__":
     main()
